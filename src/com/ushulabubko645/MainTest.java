8ef16d94b25bf51679e9336f067d88ef9ee79d0d
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2018-03-01 10:15:58 +0100

    har bare lige færdiggjordt det første objekt

--- src/com/ushulabubko645/MainTest.java
+++ src/com/ushulabubko645/MainTest.java
@@ -5,7 +5,7 @@ public class MainTest {
     public static void main(String[] args) {
 
         // jeg laver nu et par kurser som man kan gøre brug af
-        Kursus parkour = new Kursus("parkourstudiet", KursusType.fusiske_kursus, Afslutning.ingen_afslutning, Afslutningstype.ingen_prøve, "Henning", )
+        Kursus parkour = new Kursus("parkourstudiet", KursusType.fusiske_kursus, Afslutning.ingen_afslutning, Afslutningstype.ingen_prøve, "Henning", "104 og undenfor", Tidsplan.mandag_08_30_til_12_40);
 
     }
 
